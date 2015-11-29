f8112bf2e3169a2d785ce038b9fb96ca0b055803
Justus Perlwitz <mail@justusperlwitz.com>
2015-11-29 13:27:15 +0100

    Refactor main module

--- __main__.py
+++ __main__.py
@@ -18,10 +18,33 @@ request_semaphore = Semaphore(MAX_CONCURRENT)
 async def client_request(url):
     start = time()
     async with request_semaphore:
-        response = await get(url)
-        await response.text()
+        async with get(url) as response:
+            await response.text()
     duration = time() - start
     return duration
+
+
+def gen_progress_bar(tasks, loop):
+    print("In Progress")
+    loop.call_soon(progress_bar, tasks, loop)
+    print("")
+
+
+def progress_bar(requests, loop, freq=0.01):
+    width, _ = get_terminal_size()
+    done_count = len(tuple(filter(lambda t: t.done(), Task.all_tasks())))
+    tasks_left_count = requests - done_count
+    progress = int(done_count / requests * width)
+    print("\r" + "*" * progress + "." * (width - progress), end="")
+    if tasks_left_count > 0:
+        loop.call_later(freq, progress_bar, requests, loop)
+
+
+def run(tasks, loop):
+    start = time()
+    done, _ = loop.run_until_complete(tasks)
+    duration = time() - start
+    return done, duration
 
 
 def main():
@@ -29,28 +52,16 @@ def main():
         for _ in range(requests):
             yield client_request(url)
 
-    def progress_bar(freq=0.01):
-        width, _ = get_terminal_size()
-        running = int(
-            len(tuple(filter(lambda t: t.done(), Task.all_tasks()))) /
-            requests * width)
-        print("\r" + "*" * running + "." * (width - running), end="")
-        loop.call_later(freq, progress_bar)
-
     # FIXME argparse!
     url = argv[1]
     requests = int(argv[2])
     print("Requesting {} {} times".format(url, requests))
-    tasks = list(gen_tasks())
+    tasks = wait(list(gen_tasks()))
     loop = get_event_loop()
-    start = time()
-    print("In Progress")
-    loop.call_soon(progress_bar)
-    print("")
-    done, _ = loop.run_until_complete(wait(tasks))
+    gen_progress_bar(requests, loop)
+    done, duration = run(tasks, loop)
     average_task_duration = sum(
         map(lambda task: task.result(), done)) / requests
-    duration = time() - start
     print("Duration: {} seconds".format(duration))
     print("Average task duration: {} seconds".format(average_task_duration))
 
