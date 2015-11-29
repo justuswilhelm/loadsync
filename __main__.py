a0f3a219ecb4b623fcbf74729cb8d7d191debee7
Justus Perlwitz <mail@justusperlwitz.com>
2015-11-29 13:15:49 +0100

    Super awesome progress bar with semaphores!

--- __main__.py
+++ __main__.py
@@ -1,15 +1,25 @@
-from asyncio import get_event_loop, wait
+from asyncio import (
+    get_event_loop,
+    wait,
+    Semaphore,
+    Task,
+)
+from os import get_terminal_size
 from logging import basicConfig
 from sys import argv
 from time import time
 
 from aiohttp import get
 
+MAX_CONCURRENT = 5
+request_semaphore = Semaphore(MAX_CONCURRENT)
+
 
 async def client_request(url):
     start = time()
-    response = await get(url)
-    await response.text()
+    async with request_semaphore:
+        response = await get(url)
+        await response.text()
     duration = time() - start
     return duration
 
@@ -18,6 +28,15 @@ def main():
     def gen_tasks():
         for _ in range(requests):
             yield client_request(url)
+
+    def progress_bar(freq=0.01):
+        width, _ = get_terminal_size()
+        running = int(
+            len(tuple(filter(lambda t: t.done(), Task.all_tasks()))) /
+            requests * width)
+        print("\r" + "*" * running + "." * (width - running), end="")
+        loop.call_later(freq, progress_bar)
+
     # FIXME argparse!
     url = argv[1]
     requests = int(argv[2])
@@ -25,6 +44,9 @@ def main():
     tasks = list(gen_tasks())
     loop = get_event_loop()
     start = time()
+    print("In Progress")
+    loop.call_soon(progress_bar)
+    print("")
     done, _ = loop.run_until_complete(wait(tasks))
     average_task_duration = sum(
         map(lambda task: task.result(), done)) / requests
